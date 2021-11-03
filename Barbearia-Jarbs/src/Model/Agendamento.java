e25efc843619edfd97e738f39e8ec90ec8a7b49a
D-B-R <daniel.b.rocha@hotmail.com>
2021-11-03 11:38:19 -0300

    Agendamento

--- Barbearia-Jarbs/src/Model/Agendamento.java
+++ Barbearia-Jarbs/src/Model/Agendamento.java
@@ -18,6 +18,62 @@ public class Agendamento {
     private float valor;
     private Date data;
     private String observacao;
+
+    public Agendamento(int id, Cliente cliente, Servico servico, float valor, Date data) {
+        this.id = id;
+        this.cliente = cliente;
+        this.servico = servico;
+        this.valor = valor;
+        this.data = data;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Servico getServico() {
+        return servico;
+    }
+
+    public void setServico(Servico servico) {
+        this.servico = servico;
+    }
+
+    public float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public String getObservacao() {
+        return observacao;
+    }
+
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
     
     
     
